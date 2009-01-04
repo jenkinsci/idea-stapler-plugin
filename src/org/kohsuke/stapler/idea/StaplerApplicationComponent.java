63b83464f3904312924a9c2dcd92a08b961ac931
kohsuke <kohsuke@@dev.java.net>
2009-01-04 14:36:51 +0000

    registering schemas for Jelly tags

--- src/org/kohsuke/stapler/idea/StaplerApplicationComponent.java
+++ src/org/kohsuke/stapler/idea/StaplerApplicationComponent.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.lang.CompositeLanguage;
 import com.intellij.lang.StdLanguages;
+import com.intellij.javaee.ExternalResourceManager;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.kohsuke.stapler.idea.language.JellyLanguageExtension;
@@ -26,6 +27,17 @@ public class StaplerApplicationComponent implements ApplicationComponent, Inspec
             FileTypeManager fm = FileTypeManager.getInstance();
             fm.associateExtension(fm.getFileTypeByExtension("xml"),"jelly");
         }
+
+        {// register schemas for Jelly
+            ExternalResourceManager erm = ExternalResourceManager.getInstance();
+            String[] schemas = {"ant","antlr","bean","beanshell","betwixt","bsf","core","define","dynabean","email","fmt","html","http","interaction","jaxme","jetty","jface","jms","jmx","jsl","junit","log","memory","ojb","quartz","regexp","soap","sql","swing","swt","threads","util","validate","velocity","xml","xmlunit"};
+            for( String s: schemas )
+                erm.addStdResource(
+                        "jelly:"+s,  // namespace URI
+                        "/org/kohsuke/stapler/idea/resources/schemas/"+s+".xsd",
+                        getClass());
+        }
+
         // register a custom vocabulary for Jelly
 //        ((CompositeLanguage) StdLanguages.XML).registerLanguageExtension(
 //                JellyLanguageExtension.INSTANCE);
