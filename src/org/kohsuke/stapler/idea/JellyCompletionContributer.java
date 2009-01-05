17119258ff711f718580862754e73c3b71b6be43
kohsuke <kohsuke@@dev.java.net>
2009-01-05 02:27:32 +0000

    fixed auto-completion.

--- src/org/kohsuke/stapler/idea/JellyCompletionContributer.java
+++ src/org/kohsuke/stapler/idea/JellyCompletionContributer.java
@@ -6,51 +6,101 @@ import com.intellij.codeInsight.completion.CompletionProvider;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.codeInsight.lookup.LookupItem;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.XmlElementPattern;
-import com.intellij.psi.xml.XmlElement;
-import com.intellij.psi.xml.XmlToken;
 import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlElement;
 import com.intellij.psi.xml.XmlTag;
-import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlToken;
+import com.intellij.psi.xml.XmlDocument;
+import com.intellij.psi.impl.source.xml.TagNameReference;
 import com.intellij.util.ProcessingContext;
-import com.intellij.util.xml.DomManager;
+import com.intellij.xml.XmlElementDescriptor;
 import org.jetbrains.annotations.NotNull;
-import org.kohsuke.stapler.idea.psi.ref.TagReference;
-import org.kohsuke.stapler.idea.dom.model.JellyTag;
-import org.kohsuke.stapler.idea.dom.model.AttributeTag;
+import org.kohsuke.stapler.idea.descriptor.XmlNSDescriptorImpl;
 
 /**
- * Attribute name completion for Jelly tag libraries define as tag files. 
+ * Tag name completion for Jelly tag libraries defined as tag files.
+ *
+ * <p>
+ * One would think that contributing {@link XmlNSDescriptorImpl} and
+ * implementing {@link XmlNSDescriptorImpl#getRootElementsDescriptors(XmlDocument)} is enough
+ * to cause the IDEA XML module to show tag name completions, but apparently it is not.
+ *
+ * <p>
+ * The problem is when we have text like the following:
+ *
+ * <pre><xmp>
+   <j:jelly xmlns:t="/lib" xmlns:j="jelly:core">
+       <html>
+           <body>
+               <!-- hit t, ctrl+space to make sure completion kicks in  -->
+               <t:
+           </body>
+       </html>
+   </j:jelly>
+ * </xmp></pre>
+ *
+ * <p>
+ * The html/body tags get dtd.XmlElementDescriptorImpl as their {@link XmlElementDescriptor}
+ * (they represent in-memory for-this-document-only temporary DTDs), and unless the body tag
+ * appear elsewhere and have some children, it will return null from its
+ * {@link XmlElementDescriptor#getElementDescriptor(XmlTag, XmlTag)} when given &lt;t:something/>,
+ * and {@link TagNameReference}, which calls {@link XmlNSDescriptorImpl#getRootElementsDescriptors(XmlDocument)}
+ * to list up possible children, uses this as a signal that &lt;t:something/>
+ * is not a valid child in this context.
+ *
+ * <p>
+ * While the above doesn't really explain why &lt;j:*> can be completed in this context,
+ * I eventually decided that just writing another {@link CompletionContributor} on its own
+ * is the easiest thing to do here.
+ *
+ * <p>
+ * So what this {@link CompletionContributor} does is to list up all the legal
+ * Jelly tags available in the current namespace bindings as completion candidates.
  *
  * @author Kohsuke Kawaguchi
  */
 public class JellyCompletionContributer extends CompletionContributor {
     public JellyCompletionContributer() {
         extend(CompletionType.BASIC, // in case of XML completion, this always seems to be BASIC
-                XML_ATTRIBUTE_NAME_PATTERN,
+                XML_ELEMENT_NAME_PATTERN,
                 new CompletionProvider<CompletionParameters>(false, true) {
                     // REFERENCE: spring plugin adds CompletionContributor as well.
                     protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
                         XmlElement name = (XmlElement)parameters.getPosition();
 
-                        // this XmlAttribute contains a dummy name (presumably just to
-                        // keep the shape of the PSI tree.)
-                        XmlAttribute a = (XmlAttribute) name.getParent();
-                        XmlTag tag = a.getParent();
+                        // this pseudo-tag represents the tag being completed.
+                        XmlTag tag = (XmlTag) name.getParent();
+                        Module module = ModuleUtil.findModuleForPsiElement(tag);
 
-                        System.out.println("Completing attribute name inside "+tag.getName());
+                        /*
+                        When completion is invoked after text like "<p:",
+                        the following code gives us "p:".
+                        This would potentially speed up the following "knownNamespaces" loop.
+                        --------------------
+                        System.out.println("Completing tag name for "+tag.getName());
+                        PsiElement opos = parameters.getOriginalPosition();
+                        System.out.println("originalPos:"+ opos);
+                        if (opos instanceof XmlToken) {
+                            XmlToken originalToken = (XmlToken) opos;
+                            System.out.println(originalToken.getParent());
+                        }
+                        */
 
-                        // is this a reference to a tag file? If so, try to look up
-                        // definitions from there
-                        XmlFile tagFile = new TagReference(tag).resolve();
-                        if(tagFile!=null) {
-                            DomManager manager = DomManager.getDomManager(tagFile.getProject());
-                            JellyTag root = manager.getFileElement(tagFile, JellyTag.class).getRootElement();
-                            for( AttributeTag att : root.getDocumentation().getAttributes() ) {
-                                String n = att.getName().getStringValue();
-                                if(n!=null)
-                                    result.addElement(LookupItem.fromString(n));
+                        // just add every available jelly tags in the current namespaace
+                        String[] uris = tag.knownNamespaces();
+                        for (String uri : uris) {
+                            String prefix = tag.getPrefixByNamespace(uri);
+                            if(prefix!=null && prefix.length()>0)
+                                prefix+=':';
+                            XmlNSDescriptorImpl d = XmlNSDescriptorImpl.get(uri, module);
+                            if(d!=null) {
+                                for( XmlElementDescriptor e : d.getRootElementsDescriptors(null/*I'm not using this parameter*/)) {
+                                    result.addElement(LookupItem.fromString(prefix+e.getName()));
+                                }
                             }
                         }
                     }
@@ -77,4 +127,10 @@ public class JellyCompletionContributer extends CompletionContributor {
      */
     private static final ElementPattern XML_ATTRIBUTE_NAME_PATTERN =
             new XmlElementPattern(XmlToken.class) {}.withParent(XmlAttribute.class);
+
+    /**
+     * {@link ElementPattern} that matches attribute names.
+     */
+    private static final ElementPattern XML_ELEMENT_NAME_PATTERN =
+            new XmlElementPattern(XmlToken.class) {}.withParent(XmlTag.class);
 }
