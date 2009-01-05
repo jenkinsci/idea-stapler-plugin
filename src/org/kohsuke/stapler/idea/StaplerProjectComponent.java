1d9c27c8eadeed68357b77b551ae0c31cf620a60
kohsuke <kohsuke@@dev.java.net>
2009-01-05 06:19:45 +0000

    removing custom PSI and Language stuff that weren't needed after all

--- src/org/kohsuke/stapler/idea/StaplerProjectComponent.java
+++ src/org/kohsuke/stapler/idea/StaplerProjectComponent.java
@@ -3,25 +3,8 @@ package org.kohsuke.stapler.idea;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
-import com.intellij.psi.xml.XmlToken;
-import com.intellij.psi.xml.XmlFile;
-import com.intellij.psi.xml.XmlTag;
-import com.intellij.psi.xml.XmlAttribute;
-import com.intellij.pom.event.PomModelListener;
-import com.intellij.pom.event.PomModelEvent;
-import com.intellij.pom.event.PomChangeSet;
-import com.intellij.pom.xml.XmlChangeSet;
-import com.intellij.pom.xml.XmlAspect;
-import com.intellij.pom.xml.events.XmlChange;
-import com.intellij.pom.PomModelAspect;
-import com.intellij.pom.PomModel;
-import com.intellij.patterns.ElementPattern;
-import com.intellij.patterns.StandardPatterns;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.kohsuke.stapler.idea.psi.impl.ChangeVisitorImpl;
-
-import java.util.Iterator;
 
 /**
  * @author Kohsuke Kawaguchi
@@ -30,28 +13,9 @@ public class StaplerProjectComponent implements ProjectComponent {
     private ReferenceProvidersRegistry registry;
 //    private NamespaceFilter stripesNamespaceFilter;
 
-    public StaplerProjectComponent(Project project, PomModel pomModel, final XmlAspect xmlAspect) {
+    public StaplerProjectComponent(Project project) {
         registry = ReferenceProvidersRegistry.getInstance(project);
 
-        pomModel.addModelListener(new PomModelListener() {
-            public void modelChanged(PomModelEvent event) {
-                PomChangeSet changeSet = event.getChangeSet(xmlAspect);
-                if (changeSet instanceof XmlChangeSet) {
-                    XmlChangeSet xmlChangeSet = (XmlChangeSet) changeSet;
-                    for( XmlFile file : xmlChangeSet.getChangedFiles()) {
-                        if (!file.getName().endsWith(".jelly"))
-                            continue;
-
-                        for (XmlChange c : xmlChangeSet.getChanges())
-                            c.accept(ChangeVisitorImpl.INSTANCE);
-                    }
-                }
-            }
-
-            public boolean isAspectChangeInteresting(PomModelAspect aspect) {
-                return aspect == xmlAspect;
-            }
-        }, project);
     }
 
     public void projectOpened() {
