b0b9fc504aaaad120feea171799a389ec00c6877
kohsuke <kohsuke@@dev.java.net>
2009-01-08 06:42:07 +0000

    added search for super types

--- src/org/kohsuke/stapler/idea/JellyTagLibReferenceProvider.java
+++ src/org/kohsuke/stapler/idea/JellyTagLibReferenceProvider.java
@@ -8,11 +8,16 @@ import com.intellij.psi.PsiReferenceBase;
 import com.intellij.psi.PsiReferenceProvider;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiPackage;
+import com.intellij.psi.JavaDirectoryService;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.ProcessingContext;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.module.ModuleUtil;
+import com.intellij.openapi.module.Module;
 import org.apache.commons.lang.ArrayUtils;
 import org.jetbrains.annotations.NotNull;
 
@@ -86,10 +91,11 @@ public class JellyTagLibReferenceProvider extends PsiReferenceProvider {
                     TextRange.from(1,xav.getTextLength()-2)) {
                 private final String page = xav.getValue();
                 public PsiFile resolve() {
+                    JavaPsiFacade javaFacade = JavaPsiFacade.getInstance(xav.getProject());
                     if(page.startsWith("/")) {
                         // absolute
                         String pkg = page.substring(1,page.lastIndexOf('/')).replace('/','.');
-                        PsiPackage jpkg = JavaPsiFacade.getInstance(xav.getProject()).findPackage(pkg);
+                        PsiPackage jpkg = javaFacade.findPackage(pkg);
                         if(jpkg==null)  return null;
                         for (PsiDirectory dir : jpkg.getDirectories()) {
                             PsiFile f = dir.findFile(page.substring(page.lastIndexOf('/') + 1));
@@ -101,7 +107,30 @@ public class JellyTagLibReferenceProvider extends PsiReferenceProvider {
                         PsiFile f = xav.getContainingFile();
                         PsiDirectory p = f.getParent();
                         if(p==null) return null;
-                        return p.findFile(page);
+
+                        // if the file is in the same place, that's the target.
+                        PsiFile target = p.findFile(page);
+                        if(target!=null) return target;
+
+                        // maybe this Jelly file is a view of a Java class.
+                        // In that case, go up the inheritance hierarchy until we find it
+                        JavaDirectoryService jds = JavaDirectoryService.getInstance();
+                        PsiPackage pkg = jds.getPackage(p);
+                        if(pkg==null)   return null;
+
+                        Module m = ModuleUtil.findModuleForFile(f.getVirtualFile(), f.getProject());
+                        PsiClass c = javaFacade.findClass(pkg.getName(), GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(m));
+                        if(c==null)     return null;
+                        c = c.getSuperClass();
+                        for( ; c!=null; c=c.getSuperClass()) {
+                            pkg = javaFacade.findPackage(c.getQualifiedName());
+                            if(pkg!=null) {
+                                for (PsiDirectory dir : pkg.getDirectories()) {
+                                    target = dir.findFile(page);
+                                    if(target!=null) return f;
+                                }
+                            }
+                        }
                     }
                 }
 
