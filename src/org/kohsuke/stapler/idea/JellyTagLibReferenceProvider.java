7bc12d85a376e00765514e522444431ce0153623
kohsuke <kohsuke@@dev.java.net>
2009-01-08 06:42:20 +0000

    added search for super types

--- src/org/kohsuke/stapler/idea/JellyTagLibReferenceProvider.java
+++ src/org/kohsuke/stapler/idea/JellyTagLibReferenceProvider.java
@@ -131,6 +131,7 @@ public class JellyTagLibReferenceProvider extends PsiReferenceProvider {
                                 }
                             }
                         }
+                        return null;
                     }
                 }
 
