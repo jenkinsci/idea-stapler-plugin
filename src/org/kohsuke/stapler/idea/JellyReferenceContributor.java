3ff5a366765fc8ce6a17d0c395e7fa5a9730ca2c
Kohsuke Kawaguchi <kk@kohsuke.org>
2011-12-12 16:09:13 -0800

    Follow up IDEA 11 changes.

---
+++ src/org/kohsuke/stapler/idea/JellyReferenceContributor.java
@@ -0,0 +1,17 @@
+package org.kohsuke.stapler.idea;
+
+import com.intellij.patterns.StandardPatterns;
+import com.intellij.psi.PsiReferenceContributor;
+import com.intellij.psi.PsiReferenceRegistrar;
+import com.intellij.psi.xml.XmlAttributeValue;
+
+/**
+ * @author Kohsuke Kawaguchi
+ */
+public class JellyReferenceContributor extends PsiReferenceContributor {
+    @Override
+    public void registerReferenceProviders(PsiReferenceRegistrar registrar) {
+        JellyTagLibReferenceProvider p = new JellyTagLibReferenceProvider();
+        registrar.registerReferenceProvider(StandardPatterns.instanceOf(XmlAttributeValue.class), p);
+    }
+}
--- src/org/kohsuke/stapler/idea/StaplerApplicationComponent.java
+++ src/org/kohsuke/stapler/idea/StaplerApplicationComponent.java
@@ -6,6 +6,7 @@ import com.intellij.javaee.ExternalResourceManager;
 import com.intellij.lang.LanguageAnnotators;
 import com.intellij.lang.LanguageDocumentation;
 import com.intellij.lang.xml.XMLLanguage;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.fileTypes.FileTypeManager;
 import com.intellij.psi.filters.AndFilter;
@@ -31,24 +32,29 @@ public class StaplerApplicationComponent implements ApplicationComponent, Inspec
     public void initComponent() {
         FacetTypeRegistry.getInstance().registerFacetType(StaplerFacetType.INSTANCE);
 
-        {// register the .jelly extension as XML
-            FileTypeManager fm = FileTypeManager.getInstance();
-            fm.associateExtension(fm.getFileTypeByExtension("xml"),"jelly");
-        }
 
-        {// register schemas for Jelly
-            ExternalResourceManager erm = ExternalResourceManager.getInstance();
-            String[] schemas = {"ant","antlr","bean","beanshell","betwixt","bsf","core","define","dynabean","email","fmt","html","http","interaction","jaxme","jetty","jface","jms","jmx","jsl","junit","log","memory","ojb","quartz","regexp","soap","sql","swing","swt","threads","util","validate","velocity","xml","xmlunit","stapler"};
-            for( String s: schemas ) {
-                String name = "/org/kohsuke/stapler/idea/resources/schemas/" + s + ".xsd";
-                URL res = getClass().getClassLoader().getResource(name);
-                if (res==null)
-                    throw new AssertionError("Failed to find schema resource: "+name);
-                erm.addResource(
-                        "jelly:"+s,  // namespace URI
-                        res.toExternalForm());
+        ApplicationManager.getApplication().runWriteAction(new Runnable() {
+            public void run() {
+                {// register the .jelly extension as XML
+                    FileTypeManager fm = FileTypeManager.getInstance();
+                    fm.associateExtension(fm.getFileTypeByExtension("xml"),"jelly");
+                }
+
+                {// register schemas for Jelly
+                    ExternalResourceManager erm = ExternalResourceManager.getInstance();
+                    String[] schemas = {"ant","antlr","bean","beanshell","betwixt","bsf","core","define","dynabean","email","fmt","html","http","interaction","jaxme","jetty","jface","jms","jmx","jsl","junit","log","memory","ojb","quartz","regexp","soap","sql","swing","swt","threads","util","validate","velocity","xml","xmlunit","stapler"};
+                    for( String s: schemas ) {
+                        String name = "/org/kohsuke/stapler/idea/resources/schemas/" + s + ".xsd";
+                        URL res = getClass().getClassLoader().getResource(name);
+                        if (res==null)
+                            throw new AssertionError("Failed to find schema resource: "+name);
+                        erm.addResource(
+                                "jelly:"+s,  // namespace URI
+                                res.toExternalForm());
+                    }
+                }
             }
-        }
+        });
 
         // this is so that we can create an XmlFile whose getRootElement().getMetaData()
         // returns XmlNSDescriptorImpl. This is necessary to load schemas on the fly
