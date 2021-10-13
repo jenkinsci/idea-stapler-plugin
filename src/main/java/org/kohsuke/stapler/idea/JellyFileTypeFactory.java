bb014ca425576eb4ab22ad01c3751fee345478db
Denys Digtiar <duemir@gmail.com>
2021-10-13 12:29:51 +1000

    Revert "Fix #58 by registering Jelly file type"
    This reverts commit 284ff45af6b6725de419b20954bde957979a9da2.

--- src/main/java/org/kohsuke/stapler/idea/JellyFileTypeFactory.java
+++ src/main/java/org/kohsuke/stapler/idea/JellyFileTypeFactory.java
@@ -1,13 +1,13 @@
 package org.kohsuke.stapler.idea;
 
+import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
-import org.kohsuke.stapler.idea.jelly.JellyFileType;
 
 public class JellyFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer consumer) {
-        consumer.consume(JellyFileType.INSTANCE);
+        consumer.consume(XmlFileType.INSTANCE, "jelly");
     }
 }
