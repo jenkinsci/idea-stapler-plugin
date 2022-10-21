e43f292c53061435fdc6f8c68763a92fa21e5af6
Julien Greffe <jgreffe@cloudbees.com>
2022-10-21 14:41:44 +0200

    Feature 102: addtional null checks

--- src/main/java/org/kohsuke/stapler/idea/extension/AbstractStructureViewExtension.java
+++ src/main/java/org/kohsuke/stapler/idea/extension/AbstractStructureViewExtension.java
@@ -2,11 +2,13 @@ package org.kohsuke.stapler.idea.extension;
 
 import com.intellij.ide.structureView.StructureViewExtension;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.roots.TestSourcesFilter;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.search.GlobalSearchScopesCore;
 import org.jetbrains.annotations.Nullable;
+
+import static com.intellij.openapi.roots.TestSourcesFilter.isTestSources;
+import static com.intellij.psi.search.GlobalSearchScopesCore.projectProductionScope;
+import static com.intellij.psi.search.GlobalSearchScopesCore.projectTestScope;
 
 /**
  * Abstract class for jelly/java structure views
@@ -19,11 +21,13 @@ public abstract class AbstractStructureViewExtension implements StructureViewExt
      * Return current scope: test or production
      */
     protected GlobalSearchScope getCurrentScope(PsiElement element) {
-        if (TestSourcesFilter.isTestSources(element.getContainingFile().getVirtualFile(),
-                                            element.getProject())) {
-            return GlobalSearchScopesCore.projectTestScope(element.getProject());
+        if (element.getContainingFile() != null &&
+            element.getContainingFile().getVirtualFile() != null &&
+            isTestSources(element.getContainingFile().getVirtualFile(),
+                          element.getProject())) {
+            return projectTestScope(element.getProject());
         }
-        return GlobalSearchScopesCore.projectProductionScope(element.getProject());
+        return projectProductionScope(element.getProject());
     }
 
     @Override
