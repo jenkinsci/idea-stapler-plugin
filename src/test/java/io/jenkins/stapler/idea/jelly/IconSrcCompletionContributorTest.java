1068ee31c9e3debc9a13d6297751922e354d8a2c
Jan Faracik <43062514+janfaracik@users.noreply.github.com>
2024-12-22 19:39:23 +0000

    Update IconSrcCompletionContributorTest.java

--- src/test/java/io/jenkins/stapler/idea/jelly/IconSrcCompletionContributorTest.java
+++ src/test/java/io/jenkins/stapler/idea/jelly/IconSrcCompletionContributorTest.java
@@ -5,9 +5,15 @@ import com.intellij.testFramework.fixtures.BasePlatformTestCase;
 public class IconSrcCompletionContributorTest extends BasePlatformTestCase {
 
     public void testDefaultTagLibrary() {
-        assertDefaultTagLibrary("""
-                <l:icon src="symbol-<caret>
-                """, 111);
+        assertDefaultTagLibrary(
+                """
+                <?xml version="1.0" encoding="UTF-8"?>
+                <?jelly escape-by-default='true'?>
+                <j:jelly xmlns:j="jelly:core" xmlns:l="/lib/layout">
+                    <l:icon src="symbol-<caret>
+                </j:jelly>
+                """,
+                111);
     }
 
     private void assertDefaultTagLibrary(String body, int amount) {
